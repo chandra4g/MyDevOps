8e5bae395236e52f97634a92d9c1de4229211361
chandra4g <chandra4gcg@gmail.com>
2019-07-07 16:06:57 +0530

    https://github.com/chandra4g/MyDevOps.git

---
+++ src/main/java/com/unisys/App.java
@@ -0,0 +1,7 @@
+package com.unisys;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello welcome to DevOps..!!");
+	}
+}
---
+++ src/test/java/com/unisys/AppTest.java
@@ -0,0 +1,5 @@
+package com.unisys;
+
+public class AppTest {
+	
+}
